84cb662096b2a564e2ba2b8c369412d6a58ed0da
Nathan Wehr <nathan@wehrholdings.com>
2022-08-18 17:07:03 -0400

    Upgraded neopass to v2.0.0

--- Formula/neopass.rb
+++ Formula/neopass.rb
@@ -1,9 +1,9 @@
 class Neopass < Formula
   desc "A simple password manager that uses age encryption."
   homepage "https://github.com/nwehr/neopass"
-  version "1.1.0"
-  url "https://github.com/nwehr/neopass/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "b14d8eab2d169bf2ef560e92cd8f5cbe5e78844cfb5c5f1753ccd1ee031097bd"
+  version "2.0.0"
+  url "https://github.com/nwehr/neopass/archive/refs/tags/v2.0.0.tar.gz"
+  sha256 "016767ed44fb3a65107bd6de3f29fa417e84c2cdf99c02a8aa818721f15db7ef"
   license "BSD 2-Clause"
 
   depends_on "go@1.18" => :build
@@ -15,7 +15,7 @@ class Neopass < Formula
     (buildpath/"src/github.com/nwehr/neopass").install buildpath.children
     cd "src/github.com/nwehr/neopass" do
       system "go", "get", "./..."
-      system "go", "build", "-ldflags", "-X main.Version="+self.version, "-o", bin/"neopass", "cmd/client/main.go"
+      system "go", "build", "-ldflags", "-X main.version="+self.version, "-o", bin/"neopass", "cmd/client/main.go"
     end
   end
 
