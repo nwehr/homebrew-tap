7824563459f5c92f7f5d727b1bd5fe94cfa00448
Nathan Wehr <nathan@wehrholdings.com>
2022-08-18 13:50:51 -0400

    Upgraded gather to v1.0.1

--- Formula/gather.rb
+++ Formula/gather.rb
@@ -1,9 +1,9 @@
 class Gather < Formula
     desc "Gather output from multiple shell commands"
     homepage "https://github.com/nwehr/gather"
-    version "1.0.0"
-    url "https://github.com/nwehr/gather/archive/refs/tags/v1.0.0.tar.gz"
-    sha256 "dc4156435c3fa1bd5a2ace001faff21bb25e0a032f550099b6d329ee42fbe61b"
+    version "1.0.1"
+    url "https://github.com/nwehr/gather/archive/refs/tags/v1.0.1.tar.gz"
+    sha256 "84f4d4a63b89e20e5682e7a1f99c39dd01967bcd82610f90f24035148f67cb98"
     license "BSD 2-Clause"
   
     depends_on "go@1.18" => :build
@@ -14,7 +14,7 @@ class Gather < Formula
       ENV["PATH"] = "#{ENV["PATH"]}:#{buildpath}/bin"
       (buildpath/"src/github.com/nwehr/gather").install buildpath.children
       cd "src/github.com/nwehr/gather" do
-        system "go", "build", "-ldflags", "-X main.commit="+self.version, "-o", bin/"gather", "main.go"
+        system "go", "build", "-ldflags", "-X main.version="+self.version, "-o", bin/"gather", "main.go"
       end
     end
   
