777b0e6fae1279f9f0185f04ff4db7b301f340d0
Nathan Wehr <nathan@wehrholdings.com>
2022-08-19 13:37:09 -0400

    Upgraded neopass to v2.2.0

--- Formula/neopass.rb
+++ Formula/neopass.rb
@@ -1,9 +1,9 @@
 class Neopass < Formula
   desc "A simple password manager that uses age encryption."
   homepage "https://github.com/nwehr/neopass"
-  version "2.1.0"
-  url "https://github.com/nwehr/neopass/archive/refs/tags/v2.1.0.tar.gz"
-  sha256 "510a2b1ecd7f910684476292328fb52c818ca540cb17c8728afb2964b5a059e3"
+  version "2.2.0"
+  url "https://github.com/nwehr/neopass/archive/refs/tags/v2.2.0.tar.gz"
+  sha256 "28a967656df7ccf86fdfb8708f24745eb45b57c88d4fa3c159337d4046fc77ea"
   license "BSD 2-Clause"
 
   depends_on "go@1.18" => :build
